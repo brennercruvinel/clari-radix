a4156419c4a2351a27d53bee3f7fdbf19455f298
Brenner Cruvinel <brennertalks@gmail.com>
2024-06-19 03:43:13 -0300

    Add Radix components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Dialog, DialogTrigger, DialogContent, DialogClose } from '@radix-ui/react-dialog';
+import { Tooltip, TooltipTrigger, TooltipContent } from '@radix-ui/react-tooltip';
+import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from '@radix-ui/react-accordion';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dialog>
+        <DialogTrigger asChild>
+          <button>Open Dialog</button>
+        </DialogTrigger>
+        <DialogContent>
+          <p>This is a Radix Dialog!</p>
+          <DialogClose asChild>
+            <button>Close</button>
+          </DialogClose>
+        </DialogContent>
+      </Dialog>
+
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <button>Hover me</button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>This is a Radix Tooltip!</p>
+        </TooltipContent>
+      </Tooltip>
+
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger>Item 1</AccordionTrigger>
+          <AccordionContent>
+            <p>This is the content for item 1.</p>
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Item 2</AccordionTrigger>
+          <AccordionContent>
+            <p>This is the content for item 2.</p>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 }
